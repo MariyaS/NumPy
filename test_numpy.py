1a0fb8e51787d98e268c944f585653bae4d865a2
MariyaS <mariya.sadakova@gmail.com>
2016-03-15 17:05:54 +0100

    Added comments
    Added explanatory comments.

--- test_numpy.py
+++ test_numpy.py
@@ -1,9 +1,13 @@
+""" For practice on array operations and statistical analysis functions. """
+
 import numpy as np
 
+""" Use to verify or find out the version and configuration details. """
 def version_config():
     print(np.__version__)
     np.__config__.show()
 
+""" Example of a few statistical analysis functions. """
 def statistic_calculations(array):
     print "Given array: "
     print array
@@ -14,15 +18,20 @@ def statistic_calculations(array):
     print "Standard Deviation: "
     print np.std(array)
 
-
+""" Example of array operations. """
+def array_operations():
+    print "work in progress"
+    
 def test_run():
-    """ Use this to verify or find out version and config details """
+    """ Use this to verify or find out version and config details. """
     # version_config()
 
-    """ Use this for practice """
-    array = [1, 2, 3, 4, 5]
+    """ Use this for practice on statistical analysis functions. """
+    array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     statistic_calculations(array)
 
+    """ Use for array operations"""
+    array_operations()
 
 if __name__ == '__main__':
     test_run()
